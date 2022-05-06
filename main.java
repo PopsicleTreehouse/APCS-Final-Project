abe1a7625784848ab4e585aa188e05c086184b4d
Nigel P <58761175+Nigleto@users.noreply.github.com>
2022-05-06 09:26:41 -0700

    added some stuff to main class

--- main.java
+++ main.java
@@ -1,9 +1,22 @@
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONString;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class main{
-    URL url = new URL("https://financialmodelingprep.com/api/v3/income-statement/RY.TO?apikey=9e32e1c117e9206264ef7c63453dca84");
+    URL url;
 
+    public main() {
+        String urlBase = "https://financialmodelingprep.com/api/v3/income-statement/RY.TO?apikey=9e32e1c117e9206264ef7c63453dca84";
+        try {
+
+            url = new URL(urlBase);
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            
+            e.printStackTrace();
+        }
+    }
+    
 }
