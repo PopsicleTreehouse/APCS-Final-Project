d688772927a4e988a8276f7197250cf5f0a769e1
Nigel P <58761175+Nigleto@users.noreply.github.com>
2022-05-06 09:15:58 -0700

    zdfsad

---
+++ main.java
@@ -0,0 +1,9 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONString;
+
+public class main{
+    URL url = new URL("https://financialmodelingprep.com/api/v3/income-statement/RY.TO?apikey=9e32e1c117e9206264ef7c63453dca84");
+
+}
