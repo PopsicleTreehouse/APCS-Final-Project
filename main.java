e37f69fcffa0f1885ff86d0fe7379968a974970f
Nigel P <58761175+Nigleto@users.noreply.github.com>
2022-05-10 09:10:25 -0700

    added

--- main.java
+++ main.java
@@ -1,32 +1,32 @@
-import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
+// import org.json.simple.JSONObject;
+// import org.json.simple.JSONArray;
+// import java.net.MalformedURLException;
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import java.net.HttpURLConnection;
+// import java.net.MalformedURLException;
+// import java.net.URL;
+// import java.util.Map;
+// import java.util.Scanner;
 
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import org.json.simple.*;
+
 
 public class main{
     URL url;
     JSONObject obj = new JSONObject();
 
-    public main() {
-        String urlBase = "https://financialmodelingprep.com/api/v3/income-statement/RY.TO?apikey=9e32e1c117e9206264ef7c63453dca84";
-        try {
+    public static void main() {
+        // API Key is : 9e32e1c117e9206264ef7c63453dca84
+        URL url = new URL("https://financialmodelingprep.com/api/v3/profile/TSLA?apikey=9e32e1c117e9206264ef7c63453dca84");
+        
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
 
-            URL url = new URL("url");
-			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.connect();
-			if(conn.getResponseCode() == 200) {
-				//Scanner sc = new Scanner(url.openStream)
-            }
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            
-            e.printStackTrace();
         }
+
     }
     
 }
