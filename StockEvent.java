be0b77d3d9e4741eee88cac246185eac8da68b5d
Nigel P <58761175+Nigleto@users.noreply.github.com>
2022-05-11 09:36:11 -0700

    stuff

--- StockEvent.java
+++ StockEvent.java
@@ -4,8 +4,17 @@ public class StockEvent{
     private String key;
     private String value;
 
-public StockEvent(){
-    
+public StockEvent(String key, String value){
+    this.key = key;
+    this.value = value;
+}
+
+public String getKey(){
+    return key;
+} 
+
+public String getValue(){
+    return value;
 }
 
 }
