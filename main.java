85f440624420ba5378364b766a74e8a0f6986b84
Nigel P <58761175+Nigleto@users.noreply.github.com>
2022-05-11 09:44:36 -0700

    add

--- main.java
+++ main.java
@@ -18,50 +18,30 @@ import org.json.simple.parser.JSONParser;
 
 public class main{
     private URL url;
-    private JSONObject obj = new JSONObject();
     private String value;
     private String key;
 
     public static void main(String[] args) throws Exception{
-        int splitter =0;
+        int splitter = 0;
         String key = "";
         String value = "";
         // API Key is : 9e32e1c117e9206264ef7c63453dca84
         URL url = new URL("https://financialmodelingprep.com/api/v3/profile/TSLA?apikey=9e32e1c117e9206264ef7c63453dca84");
 
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
-        for (String line; (line = reader.readLine()) != null;) {
-            splitter = line.indexOf(":");
-            key = line.substring(0, splitter);
-            value = line.substring(splitter);
-            StockEvent x = new StockEvent(key, value);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                splitter = line.indexOf(":");
+                key = line.substring(0, splitter);
+                value = line.substring(splitter);
+                StockEvent x = new StockEvent(key, value);
             
-        System.out.println(line);
-  }
-}
-catch(Exception e){
-    e.printStackTrace();
-}
-        // Scanner scan = new Scanner(System.in);
-        // System.out.println("What would you like to look at?");
-        // String input = scan.nextLine();
-        
-        
-            Object obj = parser.parse(profile_url);
-            JSONArray array = (JSONArray)obj;
-
-            for(int i=0; i<array.size(); i++){
-                System.out.println(array.get(i));
-            }
-       
-
-
-        // try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
-        //     String line; 
-        //     line = reader.readLine(1);
-        //     System.out.println(line);
-        // }
-
+            System.out.println(line);
     }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+}
+
+}
     
 }
