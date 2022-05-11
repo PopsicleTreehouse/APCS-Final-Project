56a0f7147619ba96ed6df8844bb5ae94871f7650
Nigel P <58761175+Nigleto@users.noreply.github.com>
2022-05-11 08:57:41 -0700

    mr hanson help

--- main.java
+++ main.java
@@ -25,21 +25,30 @@ public class main{
         String profile_url = new String("https://financialmodelingprep.com/api/v3/profile/TSLA?apikey=9e32e1c117e9206264ef7c63453dca84");
         
         JSONParser parser = new JSONParser();
+        URL url = new URL("https://financialmodelingprep.com/api/v3/profile/TSLA?apikey=9e32e1c117e9206264ef7c63453dca84");
+
+try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+    for (String line; (line = reader.readLine()) != null;) {
+        System.out.println("Hi");
+    System.out.println(line);
+  }
+}
+catch(Exception e){
+    e.printStackTrace();
+}
+        // Scanner scan = new Scanner(System.in);
+        // System.out.println("What would you like to look at?");
+        // String input = scan.nextLine();
         
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What would you like to look at?");
-        String input = scan.nextLine();
         
-        try{
             Object obj = parser.parse(profile_url);
             JSONArray array = (JSONArray)obj;
 
             for(int i=0; i<array.size(); i++){
-                if(array.get(i).equals(input)){
-                    // idrk what to do here
-                }
+                System.out.println(array.get(i));
             }
-        }
+       
+
 
         // try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
         //     String line; 
